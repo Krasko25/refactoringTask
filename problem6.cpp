eed63bc34d994162cfc31b71dc3dddac9707fa67
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-27 23:04:01 +0300

    вынес pathBytes

--- problem6.cpp
+++ problem6.cpp
@@ -1,10 +1,13 @@
-foreach (DirectoryInfo dir in dirs.GetDirectories()) 
-{ 
-    //create folder{16} 
-    stream.Write(new byte[] { (byte)NetworkMessage.MakeDir }, 0, 1); 
-    stream.Read(new byte[1],0, 1); 
-    stream.Write(BitConverter.GetBytes(Encoding.UTF8.GetBytes( SubFolder.Replace('\\', '/') + dir.Name.Replace('\\', '/')).Length), 0, 4); 
-    stream.Write(Encoding.UTF8.GetBytes(SubFolder.Replace('\\', '/') + dir.Name.Replace('\\', '/')),0, Encoding.UTF8.GetBytes(SubFolder.Replace('\\', '/') + dir.Name.Replace('\\', '/')).Length); 
-    //send folder name 
-    stream.Read(new byte[1], 0, 1);//Ok 
-} 
+foreach (DirectoryInfo dir in dirs.GetDirectories())
+{
+    string relativePath = SubFolder.Replace('\\', '/') + dir.Name.Replace('\\', '/');
+    byte[] pathBytes = Encoding.UTF8.GetBytes(relativePath);
+
+    stream.Write(new byte[] { (byte)NetworkMessage.MakeDir }, 0, 1);
+    stream.Read(new byte[1], 0, 1);
+
+    stream.Write(BitConverter.GetBytes(pathBytes.Length), 0, 4);
+    stream.Write(pathBytes, 0, pathBytes.Length);
+
+    stream.Read(new byte[1], 0, 1); // Ok
+}
