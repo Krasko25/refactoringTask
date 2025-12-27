e91e8189f6acdc9e167120418492ade46584a3dd
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-27 23:03:40 +0300

    проверка числа через TryParse

--- problem5.cpp
+++ problem5.cpp
@@ -1,3 +1,4 @@
-Bool IsNumber (string str) { 
-    return (str.Replace ("0", "").Replace ("1", "").Replace ("2", "").Replace ("3", "").Replace ("4", "").Replace ("5", "").Replace ("6", "").Replace ("7", "").Replace ("8", "").Replace ("9", "").Length == 0); 
-} 
+bool IsNumber(string str)
+{
+    return int.TryParse(str, out _);
+}
