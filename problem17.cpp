73f00ee09cc58ead5c18b69f7ae99058ee996972
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-27 23:18:02 +0300

    вынес 5000 в константу

--- problem17.cpp
+++ problem17.cpp
@@ -1,12 +1,26 @@
-string GetTextDiv2(string text) 
-{ 
-    int mid = text.Length / 2; 
-    int r = text.IndexOf(" ", mid); if (r < 0) r = 5000; 
-    int l = text.IndexOf(" ", 0, mid); if (l < 0) l = 5000; 
-    if (r - mid > mid - l) // to left is closer 
-        mid = l; 
-    else 
-        mid = r; 
-    if (mid == 5000) return "^&nbsp" + text; 
-    return "^&nbsp" + text.Substring(0, mid) + " ^<br/^>^&nbsp" + text.Substring(mid, text.Length - mid); 
-} 
+private const int INDEX_NOT_FOUND = 5000; // Большое число, означающее "не найдено"
+string GetTextDiv2(string text)
+{
+    int mid = text.Length / 2;
+    int rightIndex = text.IndexOf(' ', mid);
+    if (rightIndex < 0) rightIndex = INDEX_NOT_FOUND;
+
+    int leftIndex = text.LastIndexOf(' ', mid);
+    if (leftIndex < 0) leftIndex = INDEX_NOT_FOUND;
+
+    if (rightIndex == INDEX_NOT_FOUND && leftIndex == INDEX_NOT_FOUND)
+    {
+        return "&nbsp" + text;
+    }
+
+    if (rightIndex - mid > mid - leftIndex)
+    {
+        mid = leftIndex;
+    }
+    else
+    {
+        mid = rightIndex;
+    }
+
+    return "&nbsp" + text.Substring(0, mid) + " <br/>&nbsp" + text.Substring(mid);
+}
