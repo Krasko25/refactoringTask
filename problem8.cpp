ce949a32c2c20589ad363b915d33514f41e9850e
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-27 23:04:47 +0300

    интерполяция строк

--- problem8.cpp
+++ problem8.cpp
@@ -1,5 +1,2 @@
-DateTime dt = DateTime.Now; 
-string h=dt.Hour.ToString().PadLeft(2,'0'); 
-string m=dt.Minute.ToString().PadLeft(2,'0'); 
-string s=dt.Second.ToString().PadLeft(2,'0'); 
-Console.WriteLine("--"+h+":"+m+":"+s+"--"); 
+DateTime dt = DateTime.Now;
+Console.WriteLine($"--{dt.Hour:00}:{dt.Minute:00}:{dt.Second:00}--");
