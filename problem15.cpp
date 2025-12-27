fe04d4d30c067025198f71936a194226b1690d5f
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-27 23:17:00 +0300

    упростил email

--- problem15.cpp
+++ problem15.cpp
@@ -1,20 +1,16 @@
-public string generateEMail() 
-{ 
-    string res; 
-    int i = PersonName.IndexOf(" "); 
-    char[] str1 = new char[i]; 
-    PersonName.CopyTo(0, str1, 0, i); 
-    string str11 = new string(str1); 
-    char[] str2 = new char[PersonName.Length - i - 1]; 
-    PersonName.CopyTo(i + 1, str2, 0, PersonName.Length-i-1); 
-    string str22 = new string(str2); 
-    res = str11.ToString() + "." + str22.ToString(); 
-    if (res.Length > 20) 
-    { 
-        str1 = new char[20]; 
-        res.CopyTo(0, str1, 0, 20); 
-        res = new string(str1); 
-    } 
-    res += "@domain.ua"; 
-    return res; 
-} 
+public string GenerateEmail()
+{
+    int spaceIndex = PersonName.IndexOf(' ');
+    if (spaceIndex <= 0) return string.Empty; // Нет имени или фамилии
+
+    string firstName = PersonName.Substring(0, spaceIndex);
+    string lastName = PersonName.Substring(spaceIndex + 1);
+    string localPart = $"{firstName}.{lastName}";
+
+    if (localPart.Length > 20)
+    {
+        localPart = localPart.Substring(0, 20); // Обрезка, может испортить данные
+    }
+
+    return localPart + "@domain.ua";
+}
