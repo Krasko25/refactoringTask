27014a1506e9c776fd0c20ea9fe36b2c9dd3f334
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-27 23:03:18 +0300

    заменил цикл на Substring

--- problem4.cpp
+++ problem4.cpp
@@ -1,3 +1,7 @@
-string destination = null; 
-for (int i = 0; i < 13; i++) 
-    destination += source[i]; 
+public static string Truncate(this string str, int maxLength)
+{
+    return str.Length <= maxLength ? str : str.Substring(0, maxLength);
+}
+
+// Использование:
+string destination = source.Truncate(13);
